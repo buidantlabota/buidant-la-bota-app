c12b001f68f03731d242156d09715e5a4ad6f8a5
marcsociats <marc.sociats@estudiantat.upc.edu>
2026-02-16 22:22:55 +0100

    Fix: actualitzat next.config per eliminar opcions deprecated

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@
 const nextConfig = {
     output: 'standalone',
     serverExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
-    experimental: {
-        serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium']
-    },
 };
 
 export default nextConfig;
